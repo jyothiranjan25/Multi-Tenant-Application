7e0cbb0dd1f688d8120026edbd843c83545f528d
kumar jyothiranjan <jyothiranjan25@gmail.com>
2024-10-06 02:12:41 +0530

    Update OpenApiConfig.java

--- src/main/java/com/example/jkpvt/Core/Swagger/OpenApiConfig.java
+++ src/main/java/com/example/jkpvt/Core/Swagger/OpenApiConfig.java
@@ -9,18 +9,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = {"org.springdoc"})
-@Import({org.springdoc.core.configuration.SpringDocConfiguration.class, org.springdoc.webmvc.core.configuration.SpringDocWebMvcConfiguration.class,
+@Import({org.springdoc.core.configuration.SpringDocConfiguration.class,
+        org.springdoc.webmvc.core.configuration.SpringDocWebMvcConfiguration.class,
         org.springdoc.webmvc.ui.SwaggerConfig.class,
         org.springdoc.core.properties.SwaggerUiConfigProperties.class,
         org.springdoc.core.properties.SwaggerUiOAuthProperties.class,
         org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class})
-public class OpenApiConfig implements WebMvcConfigurer {
+public class OpenApiConfig {
     @Bean
     public GroupedOpenApi defaultApi() {
         return GroupedOpenApi.builder()
