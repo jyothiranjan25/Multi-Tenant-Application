35bdd0b6de85cc6f30d1ca98b27a27ae2dd1a8cc
kumar jyothiranjan <jyothiranjan25@gmail.com>
2024-10-08 05:25:14 +0530

    small Bug Fixes

--- src/main/java/com/example/jkpvt/UserManagement/UserLogin/UserLoginService.java
+++ src/main/java/com/example/jkpvt/UserManagement/UserLogin/UserLoginService.java
@@ -2,7 +2,17 @@ package com.example.jkpvt.UserManagement.UserLogin;
 
 import com.example.jkpvt.UserManagement.AppUser.AppUserDAO;
 import com.example.jkpvt.UserManagement.AppUser.AppUserDTO;
+import com.example.jkpvt.UserManagement.AppUser.AppUserListener;
+import com.example.jkpvt.UserManagement.AppUser.AppUserService;
+import com.example.jkpvt.UserManagement.AppUserRoles.AppUserRoles;
+import com.example.jkpvt.UserManagement.AppUserRoles.AppUserRolesDAO;
+import com.example.jkpvt.UserManagement.AppUserRoles.AppUserRolesDTO;
+import com.example.jkpvt.UserManagement.AppUserRoles.AppUserRolesService;
+import com.example.jkpvt.UserManagement.Roles.RolesDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,23 +22,33 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-public class UserLoginService implements UserDetailsService {
+public class UserLoginService implements UserDetailsService, ApplicationContextAware {
 
-    private final AppUserDAO appUserDAO;
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        UserLoginService.applicationContext = applicationContext;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try {
             AppUserDTO appUserDTO = new AppUserDTO();
             appUserDTO.setUserName(username);
-            List<AppUserDTO> appUserDTOList = appUserDAO.get(appUserDTO);
+            List<AppUserDTO> appUserDTOList = applicationContext.getBean(AppUserService.class).get(appUserDTO);
             if (appUserDTOList.isEmpty()) {
                 throw new UsernameNotFoundException("User not found");
             }
+
+            AppUserRolesDTO appUserRolesDTO = new AppUserRolesDTO();
+            appUserRolesDTO.setAppUserId(appUserDTOList.getFirst().getId());
+            List<AppUserRolesDTO> appUserRolesList = applicationContext.getBean(AppUserRolesService.class).get(appUserRolesDTO);
+
             return User.withUsername(appUserDTOList.getFirst().getUserName())
+                    .username(appUserDTOList.getFirst().getUserName())
                     .password(appUserDTOList.getFirst().getPassword())
-                    .roles(appUserDTOList.getFirst().getIsAdmin() ? "ADMIN" : "USER")
+                    .disabled(!appUserDTOList.getFirst().getIsActive())
                     .build();
         } catch (Exception e) {
             throw new UsernameNotFoundException(e.getMessage());
