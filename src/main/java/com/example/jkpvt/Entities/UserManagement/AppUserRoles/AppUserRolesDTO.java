bf310221168d32c50ceb50be1b2871aa770ef3e5
kumar jyothiranjan <jyothiranjan25@gmail.com>
2024-11-30 11:17:37 +0530

    small change

--- src/main/java/com/example/jkpvt/Entities/UserManagement/AppUserRoles/AppUserRolesDTO.java
+++ src/main/java/com/example/jkpvt/Entities/UserManagement/AppUserRoles/AppUserRolesDTO.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Setter
 public class AppUserRolesDTO extends CommonFilterDTO<AppUserRolesDTO> {
     private RolesDTO roles;
-    private UserGroupDTO userGroupDTO;
+    private UserGroupDTO userGroupDto;
     private Long appUserId;
     private Long rolesId;
 }
